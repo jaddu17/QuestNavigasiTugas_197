1efe91dabe8b13f5f9a2f91efe4f730808dd82b8
jaddu17 <shafajaddu@gmail.com>
2025-11-08 12:49:46 +0700

    membuat class Navigasi

---
+++ app/src/main/java/com/example/tugaspertemuan6/Navigasi.kt
@@ -0,0 +1,21 @@
+package com.example.tugaspertemuan6
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tugaspertemuan6.view.FormList
+import com.example.tugaspertemuan6.view.FormPendaftaran
+import com.example.tugaspertemuan6.view.FormWelcome
+
+enum class Navigasi{
+    Home,
+    List,
+    Form
+}
