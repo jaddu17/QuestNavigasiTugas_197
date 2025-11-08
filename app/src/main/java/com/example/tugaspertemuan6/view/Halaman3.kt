d88607d0f53a841bd913281417420ed80e0e687b
jaddu17 <shafajaddu@gmail.com>
2025-11-08 12:39:05 +0700

    membuat variabel dan value

--- app/src/main/java/com/example/tugaspertemuan6/view/Halaman3.kt
+++ app/src/main/java/com/example/tugaspertemuan6/view/Halaman3.kt
@@ -27,4 +27,21 @@ import com.example.tugaspertemuan6.R
 fun FormPendaftaran(
     OnKembaliBtnClick: () -> Unit,
     OnSubmitBtnClick: () -> Unit
-)
+) {
+    var textNama by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+
+    var namaLengkap by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    val gender:List<String> = listOf("Laki-Laki", "Perempuan")
+    val kawin:List<String> = listOf("Single", "Taken", "HTS")
+
+
+}
