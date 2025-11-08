de974c6cd91379c38440d8447674a0948b27d938
jaddu17 <shafajaddu@gmail.com>
2025-11-08 12:38:34 +0700

    membuat fun FormPendaftaran

---
+++ app/src/main/java/com/example/tugaspertemuan6/view/Halaman3.kt
@@ -0,0 +1,30 @@
+package com.example.tugaspertemuan6.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugaspertemuan6.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormPendaftaran(
+    OnKembaliBtnClick: () -> Unit,
+    OnSubmitBtnClick: () -> Unit
+)
