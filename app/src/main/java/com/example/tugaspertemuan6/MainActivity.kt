9801156de55d2af010fd0e20e910ec37c0421a7c
jaddu17 <shafajaddu@gmail.com>
2025-11-08 12:54:14 +0700

    memperbaiki mainactivity

--- app/src/main/java/com/example/tugaspertemuan6/MainActivity.kt
+++ app/src/main/java/com/example/tugaspertemuan6/MainActivity.kt
@@ -6,14 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tugaspertemuan6.ui.theme.TugasPertemuan6Theme
-
+import com.example.tugaspertemuan6.view.FormList
+import com.example.tugaspertemuan6.view.FormPendaftaran
+import com.example.tugaspertemuan6.view.FormWelcome
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +25,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TugasPertemuan6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    )  {
+                        DataApp()
+                    }
             }
         }
     }
